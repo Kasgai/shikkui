9356e1492b03f661a584a37382ff0512742e0851
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2018-10-16 13:13:30 +0900

    [wip] add websocket client

---
+++ websocket.js
@@ -0,0 +1,14 @@
+const ws = new Websocket();
+
+ws.onopen = () => {
+    console.log("ok");
+}
+
+ws.onclose = () => {
+    console.log("close");
+}
+
+ws.onerror = e => {
+    console.log(`error: ${e}`)
+}
+
