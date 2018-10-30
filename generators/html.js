f90d2645e755c48d76ae2072397a16bc94572e55
Inspecode <inspecode-bot@rocro.com>
2018-10-30 03:41:50 +0000

    Fix Prettier errors

--- generators/html.js
+++ generators/html.js
@@ -367,31 +367,30 @@ HtmlGenerator["id"] = function(block) {
   return [code, HtmlGenerator.ORDER_NONE];
 };
 
-
 // Node control scripts
 
 HtmlGenerator["getelementbyid"] = function(block) {
-    var value_id_name = Blockly.JavaScript.valueToCode(
-        block,
-        "NAME",
-        Blockly.JavaScript.ORDER_ATOMIC
-    );
-    var statements_text = Blockly.JavaScript.statementToCode(block, "TEXT");
-    var code =
-        "document.getElementById(" +
-        value_id_name.trim() +
-        ")" +
-        statements_text.trim() +
-        "\n";
-    return code;
+  var value_id_name = Blockly.JavaScript.valueToCode(
+    block,
+    "NAME",
+    Blockly.JavaScript.ORDER_ATOMIC
+  );
+  var statements_text = Blockly.JavaScript.statementToCode(block, "TEXT");
+  var code =
+    "document.getElementById(" +
+    value_id_name.trim() +
+    ")" +
+    statements_text.trim() +
+    "\n";
+  return code;
 };
 
 HtmlGenerator["innerhtml"] = function(block) {
-    var value_id_name = Blockly.JavaScript.valueToCode(
-        block,
-        "NAME",
-        Blockly.JavaScript.ORDER_ATOMIC
-    );
-    var code = ".innerHTML=" + value_id_name.trim() + ";";
-    return code;
+  var value_id_name = Blockly.JavaScript.valueToCode(
+    block,
+    "NAME",
+    Blockly.JavaScript.ORDER_ATOMIC
+  );
+  var code = ".innerHTML=" + value_id_name.trim() + ";";
+  return code;
 };
