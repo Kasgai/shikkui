ae3d7d06e8efb6adb1568a9f4a6f349817405331
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2020-10-04 12:32:18 +0900

    fix: head

--- generators/block_definition.js
+++ generators/block_definition.js
@@ -91,7 +91,7 @@ const contentSectioningBlock = [
         options: [
           ["1", "h1"],
           ["2", "h2"],
-          ["3", "h2"],
+          ["3", "h3"],
           ["4", "h4"],
           ["5", "h5"],
           ["6", "h6"],
